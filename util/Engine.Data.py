7fb9ef0d774819882c4bb5ff13a0065109a8288b
Quan Pan <quanpan302@hotmail.com>
2019-12-04 17:21:29 +0100

    Update Engine.Data xr.Dataset.from_dict()

--- util/Engine.Data.py
+++ util/Engine.Data.py
@@ -1,5 +1,6 @@
 import os
 
+import datetime
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -10,8 +11,10 @@ fpath = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(fpath, '../', 'Engine', 'examples', 'Engine2.Input.nc')
 
 # xarray Dataset, define
-ds = xr.Dataset(
-    attrs={
+# ds = xr.Dataset(
+ds = xr.Dataset.from_dict(
+{
+    'attrs': {
         # meta
         'version':      'v0',                               # The version of this template NetCDF is 'v0', major release number of Github & Pypi
         'standard':     'WaterAccounting',                  # This version applies standard of 'WaterAccounting'
@@ -21,73 +24,96 @@ ds = xr.Dataset(
         'type':         'input',                            # This data is used as 'input' for engine2
         # from
         'created':      'WaterPix',                         # This data is created by 'WaterPix'
-        'time':         '2019-12-03'                        # This data is created at 'yyyy-mm-dd'
-    },
-    coords={                                                # GTiff array,  [North, West] to [South, East]
+        'time':         '2019-12-03',                       # This data is created at 'yyyy-mm-dd'
+        # GTiff format
                                                             # GTiff Origin, [West, North]
                                                             # GTiff Extent, [West, South] to [East, North] 
                                                             #               pixelWidth   = 10
                                                             #               pixelHeight  = -10
-        'longitude':    (                                   # longitude,    np.ndarray
-                            ['y', 'x'],
-                            np.array(
-                                [
-                                    [0, 10],
-                                    [0, 10],
-                                    [0, 10]
-                                ],
-                                dtype=np.float32
-                            )
-                        ),
-        'latitude':     (                                   # latitude,     np.ndarray
-                            ['y', 'x'],
-                            np.array(
-                                [
-                                    [20, 20],
-                                    [10, 10],
-                                    [0,  0]
-                                ],
-                                dtype=np.float32
-                            )
-                        ),
-        'time':         pd.date_range('2000-01-01',         # time,         datetime64
-                                      periods=2,
-                                      freq='D')
+        'originX':      0,                                  # West
+        'originY':      20,                                 # North
+        'rasterW':      10,                                 # pixel Width
+        'rasterH':      -10                                 # pixel Height
     },
-    data_vars={
-        'pcp': (                                            # variable,     dims,val,attr,encoding
-                        (                                   # dimensions,   tuple
-                            'time',
-                            'y',
-                            'x'
-                        ),
-                        np.array(                           # values,       np.ndarray
+    'dims': {
+        'time':         2,
+        'y':            3,
+        'x':            2
+    },
+    'coords': {                                             # GTiff array,  [North, West] to [South, East]
+        'lon': {                                            # longitude,    np.ndarray
+                'dims': ('y', 'x',),
+                'attrs': {
+                    'standard_name': 'longitude',
+                    'units':         'degree',
+                    'axis':          'Y'
+                },
+                'data': np.array(
+                        [
+                            [0, 10],
+                            [0, 10],
+                            [0, 10]
+                        ],
+                        dtype=np.float32
+                )
+        },
+        'lat': {                                            # latitude,    np.ndarray
+                'dims': ('y', 'x',),
+                'attrs': {
+                    'standard_name': 'latitude',
+                    'units':         'degree',
+                    'axis':          'Y'
+                },
+                'data': np.array(
+                        [
+                            [20, 20],
+                            [10, 10],
+                            [0,  0]
+                        ],
+                        dtype=np.float32
+                )
+        },
+        # 'time':         pd.date_range('2000-01-01',       # time,         datetime64
+        #                               periods=2,
+        #                               freq='D')
+        'time': {
+                'dims': ('time',),
+                'attrs': {
+                    'standard_name': 'time',
+                    'long_name':     'time'
+                },
+                'data': np.array(
+                        [
+                            datetime.datetime(2000, 1, 1, 0, 0, 0),
+                            datetime.datetime(2000, 1, 2, 0, 0, 0)
+                        ]
+                )
+        }
+    },
+    'data_vars': {
+        'pcp': {                                            # variable,     dims,val,attr,encoding
+                'dims': ('time', 'y', 'x',),
+                'attrs': {
+                    'units':    'mm/day'
+                },
+                'data': np.array(
+                        [
                             [
-                                [
-                                    [1, 2],
-                                    [3, 4],
-                                    [5, 6]
-                                ],
-                                [
-                                    [10, 20],
-                                    [30, 40],
-                                    [50, 60]
-                                ]
+                                [1, 2],
+                                [3, 4],
+                                [5, 6]
                             ],
-                            dtype=np.float32
-                        ),
-                        {                                   # attributes,   dict
-                            'units':        'mm/day'
-                        },
-                        {                                   # encoding,     dict
-                            'dtype':        np.float32,
-                            '_FillValue':   np.nan,
-                            'scale_factor': np.float32(1.0),
-                            'add_offset':   np.float32(0.0)
-                        }
-        )
+                            [
+                                [10, 20],
+                                [30, 40],
+                                [50, 60]
+                            ]
+                        ],
+                        dtype=np.float32
+                )
+        }
     }
-)
+})
 
 # xarray Dataset, write to NetCDF file
 ds.to_netcdf(
@@ -96,14 +122,23 @@ ds.to_netcdf(
     format='NETCDF4',
     engine='netcdf4',
     encoding={
-        'longitude': {
-            'dtype':    np.float32
+        'lon': {
+            'dtype':        np.float32
         },
-        'latitude': {
-            'dtype':    np.float32
+        'lat': {
+            'dtype':        np.float32
+        },
+        'pcp': {
+            'dtype':        np.float32,
+            '_FillValue':   np.nan,
+            'scale_factor': np.float32(1.0),
+            'add_offset':   np.float32(0.0)
         }
     }
 )
+
+# xarray Dataset, close NetCDF file
+ds.close()
 
 # xarray Dataset, open from NetCDF file
 with xr.open_dataset(file) as ds:
